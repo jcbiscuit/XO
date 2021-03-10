e9e215e8df159850886b687ea1f0899405ff5357
Jody Mazinyi <jodyclelland8@gmail.com>
2021-03-10 20:38:48 +1100

    Initial Commit

---
+++ XO/XOApp.swift
@@ -0,0 +1,17 @@
+//
+//  XOApp.swift
+//  XO
+//
+//  Created by Jody Clelland on 10/3/21.
+//
+
+import SwiftUI
+
+@main
+struct XOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
